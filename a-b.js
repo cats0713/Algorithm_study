02c639f8c1be486b77ffe06e5df65a6b000d4c3d
cats0713 <cats0713@naver.com>
2021-02-17 17:54:06 +0900

    study

---
+++ a-b.js
@@ -0,0 +1,14 @@
+function ab(){
+    let a = prompt('a',0);
+    let b = prompt('b',0);
+    if (a>0 && b<10) {
+        console.log(`${a} ${b}`);
+    
+        console.log(a-b);
+    }
+    else {
+        console.log(`error`);
+    }
+    
+}
+ab();
